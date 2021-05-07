386275abeee58b22821b4b1d50acb6532f436a82
dvdkwak <dvdkwak@gmail.com>
2021-05-07 23:35:34 +0200

    testing player check

--- Factions/Stormlands/Lua/StormlandsTrait.lua
+++ Factions/Stormlands/Lua/StormlandsTrait.lua
@@ -1,4 +1,18 @@
 -- Lua Script1
--- Author: Gerbus
+-- Author: dvdkwak
 -- DateCreated: 5/7/2021 11:06:36 PM
 --------------------------------------------------------------
+
+-- Trait of the Stormland increases the combat bonus of all units by 10% per DoF (Decleration of Friendship)
+
+function StormLandsTrait( playerID ){
+
+    local player = Players[playerID];
+
+    print("this is a test");
+    print("CURRENT CIV: " .. player:GetCivilizationType());
+
+} -- end of StormLandTrait
+
+-- on turn start, call this method
+GameEvent.PlayerDoTurn.add( StormLandsTrait );
