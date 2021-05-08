1f35a2c0027250a8e6aead8688f37b63fe90bf51
dvdkwak <dvdkwak@gmail.com>
2021-05-08 02:23:50 +0200

    test

--- Factions/Stormlands/Lua/StormlandsTrait.lua
+++ Factions/Stormlands/Lua/StormlandsTrait.lua
@@ -21,7 +21,7 @@ function StormLandsTrait( playerID )
             end
             -- if the unit has not a promotion yet, we grand it (no decleration of friendship check yet)
             if ( not unit:IsHasPromotion("PROMOTION_FURY_I") ) then
-                unit:Promote("PROMOTION_FURY_I", unit:GetLeaderUnitType());
+                unit:SetHasPromotion("PROMOTION_FURY_I", 1);
                 print("TEST PROMOTION GIVEN: UNIT SHOULD HAVE BEEN GRANTED A PROMOTION");
             end
         end
