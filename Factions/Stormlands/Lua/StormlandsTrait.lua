932653a2c824785433cc5fb6c6f8b9a949e3cd29
dvdkwak <dvdkwak@gmail.com>
2021-05-11 21:36:00 +0200

    testt

--- Factions/Stormlands/Lua/StormlandsTrait.lua
+++ Factions/Stormlands/Lua/StormlandsTrait.lua
@@ -16,7 +16,7 @@ function StormLandsTrait( playerID )
     if ( player:IsAlive() and civType == GameInfoTypes["CIVILIZATION_STORMLANDS"] ) then
 
         -- calculate number of DoF of the player
-        for player in players do
+        for player in Players do
             print("Player with ID: " .. player:GetID() .. " is in the game");
         end
 
