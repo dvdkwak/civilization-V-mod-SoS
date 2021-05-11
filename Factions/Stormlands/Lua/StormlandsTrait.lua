390712c35843e979abbc867320854e5cb253fbfa
dvdkwak <dvdkwak@gmail.com>
2021-05-11 20:45:40 +0200

    test

--- Factions/Stormlands/Lua/StormlandsTrait.lua
+++ Factions/Stormlands/Lua/StormlandsTrait.lua
@@ -13,28 +13,34 @@ function StormLandsTrait( playerID )
     local civType = player:GetCivilizationType();
     local stormPromotionI = GameInfoTypes["PROMOTION_FURY_I"];
     local stormPromotionII = GameInfoTypes["PROMOTION_FURY_II"];
+    local stormPromotionIII = GameInfoTypes["PROMOTION_FURY_III"];
 
     -- when the civ is stormlands, initiate bonus on all units
     if ( player:IsAlive() and civType == GameInfoTypes["CIVILIZATION_STORMLANDS"] ) then
-        if ( Locale.ConvertTextKey("DEBUG_MODE") ) then
-            print("TEST: YOU SHOULD GET A COMBAT BONUS FOR EVERY UNIT");
-        end
         for unit in player:Units() do
-            if ( Locale.ConvertTextKey("DEBUG_MODE") ) then
-                print("TEST UNIT: " .. unit:GetID());
-            end
-            
-            -- if the unit has not a promotion yet, we grand it (no decleration of friendship check yet)
-            if ( not unit:IsHasPromotion(stormPromotionI) ) then
-                unit:SetHasPromotion(stormPromotionI, true);
-                print("TEST PROMOTION GIVEN: UNIT SHOULD HAVE BEEN GRANTED PROMOTION_FURY_I");
+
+            -- if the unit has promotion 2, give it promotion 3 (TEST)
+            if ( unit:IsHasPromotion(stormPromotionI) ) then
+                unit:SetHasPromotion(stormPromotionI, false);
+                unit:SetHasPromotion(stormPromotionII, false);
+                unit:SetHasPromotion(stormPromotionIII, true);
             end
 
             -- if the unit has promotion 1, give it promotion 2 (TEST)
             if ( unit:IsHasPromotion(stormPromotionI) ) then
+                unit:SetHasPromotion(stormPromotionI, false);
                 unit:SetHasPromotion(stormPromotionII, true);
+                unit:SetHasPromotion(stormPromotionIII, false);
             end
-        end
+
+            -- if the unit has not a promotion yet, we grand it (no decleration of friendship check yet)
+            if ( not unit:IsHasPromotion(stormPromotionI) ) then
+                unit:SetHasPromotion(stormPromotionI, true);
+                unit:SetHasPromotion(stormPromotionII, false);
+                unit:SetHasPromotion(stormPromotionIII, false);
+            end
+
+        end -- end of for unit
     end
 
 end -- end of StormLandTrait
