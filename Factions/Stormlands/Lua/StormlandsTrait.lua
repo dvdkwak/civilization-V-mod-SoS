c6df8bdbeb684a71ed5805034bb5abf2f4e2a7ee
dvdkwak <dvdkwak@gmail.com>
2021-05-16 23:32:48 +0200

    Stormlands trait version 1

--- Factions/Stormlands/Lua/StormlandsTrait.lua
+++ Factions/Stormlands/Lua/StormlandsTrait.lua
@@ -11,43 +11,78 @@ function StormLandsTrait( playerID )
     local civType = player:GetCivilizationType();
     local stormPromotionI = GameInfoTypes["PROMOTION_FURY_I"];
     local stormPromotionII = GameInfoTypes["PROMOTION_FURY_II"];
+    local stormPromotionIII = GameInfoTypes["PROMOTION_FURY_III"];
+    local stormPromotionIV = GameInfoTypes["PROMOTION_FURY_IV"];
+    local stormPromotionV = GameInfoTypes["PROMOTION_FURY_V"];
 
     -- when the civ is stormlands, initiate bonus on all units
     if ( player:IsAlive() and civType == GameInfoTypes["CIVILIZATION_STORMLANDS"] ) then
+        
+        -- memory for the number of DoF's
+        local numDoF = 0;
 
-        -- calculate number of DoF of the player
-        for key, value in ipairs(Players) do
-            local something;
-            print("I am a line representing a player: " ... something);
+        -- for every player in game check if there is a DoF with that player
+        for id, checkPlayer in pairs(Players) do
+            if( checkPlayer:IsEverAlive() and checkPlayer:GetCivilizationShortDescription() ~= "The Stormlands" and checkPlayer:IsPlayable()) then
+                if( player:IsDoF(id) ) then
+                    print("You have a DoF with " .. checkPlayer:GetCivilizationShortDescription() .. "! (id: " .. id .. ")");
+                    -- count number of DoF
+                    numDoF = numDoF + 1;
+                end
+            end
         end
 
-
+        -- give each unit the promotion by the number of DoF's
         for unit in player:Units() do
 
-            -- if the unit has promotion 2, give it promotion 3 (TEST)
-            if ( unit:IsHasPromotion(stormPromotionII) ) then
+            -- On 0 DoF's strip all promotions
+            if( numDoF < 1 ) then
                 unit:SetHasPromotion(stormPromotionI, false);
                 unit:SetHasPromotion(stormPromotionII, false);
-                unit:SetHasPromotion(stormPromotionIII, true);
-
-                print("Unit should have gained the stormPromotionIII");
-
-            -- if the unit has promotion 1, give it promotion 2 (TEST)
-            elseif ( unit:IsHasPromotion(stormPromotionI) ) then
-                unit:SetHasPromotion(stormPromotionI, false);
-                unit:SetHasPromotion(stormPromotionII, true);
                 unit:SetHasPromotion(stormPromotionIII, false);
+                unit:SetHasPromotion(stormPromotionIV, false);
+                unit:SetHasPromotion(stormPromotionV, false);
 
-                print("Unit should have gained the stormPromotionII");
-
-            -- if the unit has not a promotion yet, we grand it (no decleration of friendship check yet)
-            elseif ( not unit:IsHasPromotion(stormPromotionI) ) then
+            -- On 1 DoF get promotion I and strip the rest
+            elseif( numDoF == 1 ) then
                 unit:SetHasPromotion(stormPromotionI, true);
                 unit:SetHasPromotion(stormPromotionII, false);
                 unit:SetHasPromotion(stormPromotionIII, false);
+                unit:SetHasPromotion(stormPromotionIV, false);
+                unit:SetHasPromotion(stormPromotionV, false);
 
-                print("Unit should have gained the stormPromotionI");
-            end
+            -- On 2 DoF get promotion I and strip the rest
+            elseif( numDoF == 2 ) then
+                unit:SetHasPromotion(stormPromotionI, false);
+                unit:SetHasPromotion(stormPromotionII, true);
+                unit:SetHasPromotion(stormPromotionIII, false);
+                unit:SetHasPromotion(stormPromotionIV, false);
+                unit:SetHasPromotion(stormPromotionV, false);
+
+            -- On 3 DoF get promotion I and strip the rest
+            elseif( numDoF == 3 ) then
+                unit:SetHasPromotion(stormPromotionI, false);
+                unit:SetHasPromotion(stormPromotionII, false);
+                unit:SetHasPromotion(stormPromotionIII, true);
+                unit:SetHasPromotion(stormPromotionIV, false);
+                unit:SetHasPromotion(stormPromotionV, false);
+
+            -- On 4 DoF get promotion I and strip the rest
+            elseif( numDoF == 4 ) then
+                unit:SetHasPromotion(stormPromotionI, false);
+                unit:SetHasPromotion(stormPromotionII, false);
+                unit:SetHasPromotion(stormPromotionIII, false);
+                unit:SetHasPromotion(stormPromotionIV, true);
+                unit:SetHasPromotion(stormPromotionV, false);
+
+            -- On 1 DoF get promotion I and strip the rest
+            elseif( numDoF >= 5 ) then
+                unit:SetHasPromotion(stormPromotionI, false);
+                unit:SetHasPromotion(stormPromotionII, false);
+                unit:SetHasPromotion(stormPromotionIII, false);
+                unit:SetHasPromotion(stormPromotionIV, false);
+                unit:SetHasPromotion(stormPromotionV, true);
+            end -- end of elseif list
 
         end -- end of for unit
 
