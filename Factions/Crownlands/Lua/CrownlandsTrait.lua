de5e4ed046e80d4b195bd4fcddbc8748ef49f488
dvdkwak <dvdkwak@gmail.com>
2021-05-27 21:36:31 +0200

    initial commit

--- Factions/Crownlands/Lua/CrownlandsTrait.lua
+++ Factions/Crownlands/Lua/CrownlandsTrait.lua
@@ -2,3 +2,14 @@
 -- Author: dvdkwak
 -- DateCreated: 5/27/2021 9:01:13 PM
 --------------------------------------------------------------
+
+-- In here the trait of the Crownlands is defined.
+-- The Crownlands can force citystates to pledge their allegiance, which means engaging a alliance.
+-- This can be achieved when:
+--  - the city state has no other allies yet
+--  - at least 3 military units are next to the border of the citystate
+--  - The citystate is "feared" (military power is big enough)
+
+function CrownlandsTrait()
+
+end
